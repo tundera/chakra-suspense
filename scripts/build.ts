9e84545734c8fcdf38d1f8cc726c8802f32fa8b8
Philip Johnston <tunderadev@gmail.com>
2022-04-04 15:58:44 -0400

    build(scripts): 👷  adding build script
    Chaning infra/adding build script

---
+++ scripts/build.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+import { execa } from 'execa'
+
+import { getErrorMessage } from 'catch-safe'
+
+async function main() {
+  try {
+    await execa('pnpm', ['prisma', 'generate'], {
+      stdio: 'inherit',
+      env: {
+        NODE_ENV: 'production',
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+    })
+  } catch (err) {
+    throw new Error(`Failed to generate Prisma schema: ${getErrorMessage(err)}`)
+  }
+
+  try {
+    await execa('pnpm', ['prisma', 'migrate', 'deploy'], {
+      stdio: 'inherit',
+      env: {
+        NODE_ENV: 'production',
+        DATABASE_URL: process.env.MIGRATE_DATABASE_URL,
+      },
+    })
+  } catch (err) {
+    throw new Error(`Failed to migrate Prisma schema: ${getErrorMessage(err)}`)
+  }
+
+  try {
+    await execa('pnpm', ['next', 'build'], {
+      stdio: 'inherit',
+    })
+  } catch (err) {
+    throw new Error(`Failed to build Next.js application: ${getErrorMessage(err)}`)
+  }
+}
+
+main()
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
+  .finally(() => {
+    process.exit(0)
+  })
