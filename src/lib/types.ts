7b8ba6ffebfa342a1a63fe9e9bbc2f2c94475880
tundera <tunderadev@gmail.com>
2022-04-05 21:41:45 +0000

    fix(layouts): 🏷️  clean up layout getter types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,3 @@
 import type { ReactElement, ReactNode } from 'react'
 
-export type GetLayoutFunction = (page: ReactElement, pageProps?: any) => ReactNode
+export type GetLayoutFunction = (page: ReactElement) => ReactNode
